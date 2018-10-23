05cadd85d028ff61788a711bad73cabf2919482f
Mochamad Teguh Subarkah <36868806+mteguhsubarkah@users.noreply.github.com>
2018-10-23 11:42:09 +0700

    Update SPC2_0.ino

--- SPC/SPC2_0.ino
+++ SPC/SPC2_0.ino
@@ -1,6 +1,9 @@
-/*
-1. Mukhtar Amin 
-2. Mochamad Teguh Subarkah
+/* 
+Syergie Propeller Control
+-------------------------
+Mukhtar Amin
+Mochamad Teguh Subarkah
+Aji Chairul Anwar
 */
 
 //Wiring Guide
@@ -9,8 +12,6 @@
 
 //LIBRARIES
 #include <SPI.h>
-#include <Ethernet.h>
-#include <PubSubClient.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include "Syergie.h"
@@ -30,16 +31,33 @@
 #include <task.h>
 #include <timers.h>
 
-//PIN DEFINITIONS
-#define pwm_right   3
-#define pwm_left    5
-#define direct_valve_1_1 8
-#define direct_valve_1_2 9
+//Serial Comunication Transmit Delay
+#define tdelay 1
 
-#define pin_position_out  A0
-#define pinRPM_1          A1
-#define pinRPM_2          A2
-#define pin_level_tanki   A3
+//PIN DEFINITIONS
+#define pwm_flow   3
+#define pwm_pressure    5
+#define direct_valve_1_1 2
+#define direct_valve_1_2 3
+
+#define pin_level_tanki   A0
+#define pin_steer         A1
+#define pin_setNol        A2
+#define pinRPM_engine     A3
+#define pinRPM_Prop       A4
+#define pinRPM_pump       A5
+
+//DEFINITIONS FOR SERIAL COMMUNICATION ESP ID
+#define rpm_engine_ID 'R'
+#define rpm_propeller_ID 'P'
+#define rpm_pump_ID 'M'
+#define tangki_ID 'T'
+#define steer_ID 'A'
+#define azimuth_ID 'A'
+#define speed_ID 'S'
+#define Kp_ID 'P'
+#define Ki_ID 'I'
+#define Kd_ID 'D'
 
 /*-----( Define LCD )-----*/
 // set the LCD address to 0x27 for a 20 chars 4 line display
@@ -48,33 +66,23 @@
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 
 // Define Tasks
+void TaskSerial( void *pvParameters );
 void TaskRpmMeasurement( void *pvParameters );
-void TaskMQTT( void *pvParameters );
 void task_position_control( void *pvParameters );
+void task_speed_control( void *pvParameters );
 void TaskLCD( void *pvParameters );
 void TaskRPM( void *pvParameters );
 
-// Update these with values suitable for your network.
-byte mac[] = { 0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
-IPAddress ip(123, 45, 0, 12);
-IPAddress server(123, 45, 0, 10);
-
-EthernetClient ethClient;
-PubSubClient client(ethClient);
-
 /* Fuel Tank Level, Steer, Depth, and PWM*/
-int level_tanki = 0;
+int level_tangki = 0;
 int steer = 0;
 int depth = 0;
 
 int pwm1;
 int pwm2;
 
-//Buffers for Publishing via MQTT
-char send_rpm_engine[10];
-char send_rpm_propeller[10];
-char send_tanki[10];
-char send_Speed[10];
+//Buffers for Serial Communication ESP
+char st[10];
 
 //Buffers for Displaying to LCD
 char lcd_buffer0[21];
@@ -85,14 +93,11 @@ char lcd_buffer3[21];
 /* RPM Measurement */
 Proximity speed_1;
 Proximity speed_2;
-int rpm_1 = 0;
-int rpm_2 = 0;
+Proximity speed_3;
+int rpm_engine = 0;
+int rpm_prop = 0;
+int rpm_pump = 0;
 int Speed = 1500;
-int pwm_out ;
-
-/* Position Control */
-int Position = 1500;
-
 
 //PID CONSTANTS
 double P = 2.5209;
@@ -106,13 +111,16 @@ double ITerm;
 double DTerm;
 double PIDTerm;
 double Ts = 1;
+double pwm_out;
 
 double cepat = 0;
 double position_out = 0 ;
 double position_in = 500;
-double position_in_angle = 0;
 double Error; // perbedaan feedback dengan input
 double Sum = 0; // hasil integral error
+
+char buff[100];
+int i=0,counter=0,idx=0;
 //END OF PID CONSTANTS
 
 void setPWMfrequency0(int freq){
@@ -125,83 +133,18 @@ void setPWMfrequency2(int freq){
    TCCR2B = TCCR2B & 0b11111000 | freq ;
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print(F("Message arrived ["));Serial.print(topic);Serial.print(F("] "));
-  char data_in[100];
-  int value = 0;
-  for (int i=0;i<length;i++) { //storing payload to integer value
-    data_in[i] = (char)payload[i];
-    Serial.print((char)payload[i]);
-    value = value*10;
-    value=value+(payload[i]-48);
-  }
-  Serial.println();
-  
-  if(strcmp(topic,"spc_speed1") == 0){
-    client.publish("confirm_SPC1","Received");
-    Serial.println("Masuk Speednya Pak Eko");
-    Speed = value;
-  }
-
-  if(strcmp(topic,"spc_steer1") == 0){
-    client.publish("confirm_SPC1","Received");
-    Serial.println("Masuk Steernya Pak Eko");
-    Position = value;
-  }
-    
-   if(strcmp(topic,"MainControl") == 0){
-    client.publish("1EngineSpeed",dtostrf(rpm_1, 4, 0, send_rpm_engine));
-    client.publish("1PropellerSpeed",dtostrf(rpm_2, 4, 0, send_rpm_propeller));
-    client.publish("1Fuel",dtostrf(level_tanki, 2, 0, send_tanki));
-    client.publish("1Box","connected");
-  }
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print(F("Attempting MQTT connection... "));
-    // Attempt to connect
-    if (client.connect("Box1")) {
-      Serial.print(server);
-      Serial.println(F(" connected"));
-      // Once connected, publish an announcement...
-      // ... or not...
-      // ... and resubscribe
-      client.subscribe("spc_speed1");
-      client.subscribe("spc_steer1");
-      client.subscribe("steer1_kp");
-      client.subscribe("steer1_ki");
-      client.subscribe("steer1_kd");
-      client.subscribe("MainControl");
-    } else {
-      Serial.print(F("failed, rc="));
-      Serial.print(client.state());
-      Serial.println(F(" try again in 5 seconds"));
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
 // the setup function runs once when you press reset or power the board
 void setup() {  
-  speed_1.pinRPM(pinRPM_1);
-  speed_2.pinRPM(pinRPM_2);
-  
-  // initialize serial communication at 115200 bits per second:
+  speed_1.pinRPM(pinRPM_engine);
+  speed_2.pinRPM(pinRPM_Prop);
+  speed_3.pinRPM(pinRPM_pump);
+  // initialize serial for debugging
+    (115200);
+  // initialize serial for UART ESP module
   Serial.begin(115200);
+  Serial2.begin(115200);
+  Serial3.begin(115200);
   
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
-  }
-
-  client.setServer(server, 1883);
-  client.setCallback(callback);
-  Ethernet.begin(mac, ip);
-  
-  // Allow the hardware to sort itself out
-  delay(1500);
   
   lcd.begin(20,4);         // initialize the lcd for 20 chars 4 lines, turn on backlight
 
@@ -235,8 +178,8 @@ void setup() {
     ,  NULL );
 
   xTaskCreate(
-    TaskMQTT
-    ,  (const portCHAR *) "MQTT"
+    TaskSerial
+    ,  (const portCHAR *) "SERIAL"
     ,  1024  // Stack size
     ,  NULL
     ,  1  // Priority
@@ -244,20 +187,21 @@ void setup() {
 
   xTaskCreate(
     task_position_control
-    ,  (const portCHAR *) "PID"
+    ,  (const portCHAR *) "PositionControl"
     ,  1024  // Stack size
     ,  NULL
     ,  1  // Priority
     ,  NULL );
 
-    xTaskCreate(
+    
+  xTaskCreate(
     task_speed_control
-    ,  (const portCHAR *) "PID"
+    ,  (const portCHAR *) "SpeedControl"
     ,  1024  // Stack size
     ,  NULL
     ,  1  // Priority
     ,  NULL );
-    
+     
   xTaskCreate(
     TaskLCD
     ,  (const portCHAR *) "LCD"
@@ -280,6 +224,82 @@ void setup() {
 void loop(){} // Empty. Things are done in Tasks. 
 
 /*---------------------- Tasks ---------------------*/
+
+void TaskSerial(void *pvParameters)  // Task Serial
+{
+  (void) pvParameters;
+  TickType_t xLastWakeTime;
+  xLastWakeTime = xTaskGetTickCount();
+  
+  int countC, countD;
+  for (;;)
+  {
+    countD = millis() - countC; countC = millis();
+//    Serial.print(F("Time TaskMQT: "));Serial.println(countD);
+    
+    rpm_engine = speed_1.calcRPM();
+    rpm_prop = speed_2.calcRPM();
+    rpm_pump = speed_3.calcRPM();
+    if (Serial2.available()) {
+      buff[i] = Serial2.read();
+      if(buff[i]=='\n'){
+          i=-1;
+          Serial.print("Serial Message arrived ");
+          Serial.print(buff);
+          counter = 0;
+          dtostrf(0, 100, 0, buff);
+          //Serial.println();
+      }
+      else if(buff[i]=='\t'){
+          idx=0;
+          dtostrf(0, 10, 0, st);
+      }
+      else if(buff[i]==speed_ID){
+          //Serial.print("Speed = ");
+          Speed = atoi(st);
+          //Serial.println(Speed);
+      }
+      else if(buff[i]==steer_ID){
+          Serial.print("Steer = ");
+          Serial.println(atoi(st));
+      }
+      else if(buff[i]==Kp_ID){
+          Serial.print("Kp = ");
+          P = atof(st);
+          Serial.println(P);
+      }
+      else if(buff[i]==Kd_ID){
+          Serial.print("Kd = ");
+          D  = atof(st);
+          Serial.println(D);
+      }
+      else if(buff[i]==Ki_ID){
+          Serial.print("Ki = ");
+          I = atof(st);
+          Serial.println(I);
+      }
+      st[idx]=buff[i];
+      i++;
+      idx++;
+    }
+    else{
+      Serial.print("No Data Serial #");
+      Serial.println(counter);
+    }
+
+    Serial3.print("\t");
+    Serial3.print(rpm_engine);Serial3.print(rpm_engine_ID);Serial3.print("\t");
+    Serial3.print(rpm_prop);Serial3.print(rpm_propeller_ID);Serial3.print("\t");
+    Serial3.print(rpm_pump);Serial3.print(rpm_pump_ID);Serial3.print("\t");
+    Serial3.print(level_tangki);Serial3.print(tangki_ID);Serial3.print("\t");
+    Serial3.print(analogRead(position_out));Serial3.print(azimuth_ID);Serial3.println("\t");
+
+    counter++;
+    
+    vTaskDelayUntil( &xLastWakeTime, 10);
+  }
+}
+
 void TaskRpmMeasurement(void *pvParameters)  // Task for RPM and Measurements
 {
   (void) pvParameters;
@@ -292,64 +312,13 @@ void TaskRpmMeasurement(void *pvParameters)  // Task for RPM and Measurements
     countB = millis() - countA; countA = millis();
 //    Serial.print(F("Time TaskRPM: "));Serial.println(countB);
     
-    rpm_1 = speed_1.calcRPM();
-    rpm_2 = speed_2.calcRPM();
-
-    //Steer dan Depth masih dummy
-    steer += 10;
-    if (steer > 360) {
-      steer = 0;
-    }
-    depth += 13;
-    if (depth > 99) {
-      depth = 0;
-    }
-    
-    Serial.print(F("RPM 1 : "));Serial.println(rpm_1);
-    Serial.print(F("RPM 2 : "));Serial.println(rpm_2);
-    
-    //Fuel Measurement
-    level_tanki = map(analogRead(pin_level_tanki),0,1023,0,20);
-    Serial.print(F("Level Tangki : "));Serial.println(level_tanki);
-    
-    //Governor Control
-    switch(Speed){
-      case 1900: pwm1 = 200; pwm2 = 0; break; //Speed UP
-      case 1500: pwm1 = 0; pwm2 = 0; break; //Steady
-      case 1300: pwm1 = 0; pwm2 = 200; break; //Speed Down
-    }
-    Serial.print(F("Speed : "));Serial.println(Speed);
-    Serial.print(F("PWM 1 : "));Serial.println(pwm1);
-    Serial.print(F("PWM 2 : "));Serial.println(pwm2);
-    analogWrite(pwm_right, pwm1);
-    analogWrite(pwm_left, pwm2);
-    
+    rpm_engine = speed_1.calcRPM();
+    rpm_prop = speed_2.calcRPM();
+    rpm_pump = speed_3.calcRPM();
     vTaskDelayUntil( &xLastWakeTime, 10);
   }
 }
 
-void TaskMQTT(void *pvParameters)  // Task MQTT
-{
-  (void) pvParameters;
-  TickType_t xLastWakeTime;
-  xLastWakeTime = xTaskGetTickCount();
-  
-  int countC, countD;
-  for (;;)
-  {
-    countD = millis() - countC; countC = millis();
-//    Serial.print(F("Time TaskMQT: "));Serial.println(countD);
-    
-    rpm_1 = speed_1.calcRPM();
-    rpm_2 = speed_2.calcRPM();
-    if (!client.connected()) {
-      reconnect();
-    }
-    client.loop();
-    
-    vTaskDelayUntil( &xLastWakeTime, 10);
-  }
-}
 
 void task_position_control(void *pvParameters)  // Task PID
 {
@@ -360,14 +329,13 @@ void task_position_control(void *pvParameters)  // Task PID
   int countE, countF;
   for (;;)
   {
-    
     countF = millis() - countE; countE = millis();
-//    Serial.print(F("Time task_position_control: "));Serial.println(countF);
+//    Serial.print(F("Time TaskPID: "));Serial.println(countF);
 
-    rpm_1 = speed_1.calcRPM();
-    rpm_2 = speed_2.calcRPM();
-
-    double position_out = analogRead(pin_position_out);
+    rpm_engine = speed_1.calcRPM();
+    rpm_prop = speed_2.calcRPM();
+    rpm_pump = speed_3.calcRPM();
+    double position_out = analogRead(pin_steer);
     // Perhitungan PID
     Error = position_in - position_out;
     Buff = position_out; // untuk mencari derivatif
@@ -383,10 +351,9 @@ void task_position_control(void *pvParameters)  // Task PID
       PIDTerm = -255;
   
     cepat = PIDTerm; // Hasil PID dijadikan data kecepatan
-    position_in_angle = map(Position, 1100,1900,0,360);
-    position_in = map(position_in, 0,360,250,750);
-    Serial.print("Position in :");Serial.println(position_in);
-    Serial.print("Position_out : ");Serial.println(analogRead(position_out));
+    //Serial.print("Position in :");
+    
+    //Serial.print("Position_out : ");
   
     // Jika nilai kecepatan (-), stir berputar ke kanan
     if (cepat < 0) {
@@ -423,6 +390,8 @@ void task_speed_control(void *pvParameters)  // Task PID
     
     pwm_out = map(Speed,1100,1900,0,255);
     Serial.println(pwm_out);
+    analogWrite(4,255);
+    analogWrite(5,pwm_out);
 
     
     // Delay sebesar 11 tick ~ 198 ms
@@ -442,12 +411,13 @@ void TaskLCD(void *pvParameters)  // Task LCD
     countH = millis() - countG; countG = millis();
 //    Serial.print(F("Time TaskLCD: "));Serial.println(countH);
 
-    rpm_1 = speed_1.calcRPM();
-    rpm_2 = speed_2.calcRPM();
-
-    sprintf((char*) lcd_buffer0, "RPM Engine: %4d    ", rpm_1);
-    sprintf((char*) lcd_buffer1, "RPM Propeller: %4d ", rpm_2);
-    sprintf((char*) lcd_buffer2, "Level Tangki: %2d L  ", level_tanki);
+    rpm_engine = speed_1.calcRPM();
+    rpm_prop = speed_2.calcRPM();
+    rpm_pump = speed_3.calcRPM();
+    level_tangki = map(analogRead(pin_level_tanki),0,1023,0,20);
+    sprintf((char*) lcd_buffer0, "RPM Engine: %4d    ", rpm_engine);
+    sprintf((char*) lcd_buffer1, "RPM Propeller: %4d ", rpm_prop);
+    sprintf((char*) lcd_buffer2, "Level Tangki: %2d L  ", level_tangki);
     sprintf((char*) lcd_buffer3, "Steer:%3d%c Depth:%2dm", steer, 223, depth);
 
     lcd.setCursor(0,0); //Start at character 0 on line 0
@@ -473,9 +443,9 @@ void TaskRPM(void *pvParameters)  // Task khusus RPM
   int countG, countH;
   for (;;)
   {
-    rpm_1 = speed_1.calcRPM();
-    rpm_2 = speed_2.calcRPM();
-
+    rpm_engine = speed_1.calcRPM();
+    rpm_prop = speed_2.calcRPM();
+    rpm_pump = speed_3.calcRPM();
     vTaskDelayUntil( &xLastWakeTime, 1);
   }
 }
